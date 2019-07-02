5b68200fc648774ce0fd3b690a2349d1e9bf60d0
Farhan Fuad <farhan.fuad@synesisit.com.bd>
2019-07-02 19:27:54 +0600

    Initial Commit with c3p0

---
+++ src/main/java/com/hibernet/HibernateUtil.java
@@ -0,0 +1,73 @@
+package com.hibernet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+
+
+import com.hibernet.entity.person;
+
+public class HibernateUtil
+{
+    private static StandardServiceRegistry registry;
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory()
+    {
+        if (sessionFactory == null)
+        {
+            try {
+                StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder();
+
+                Map<String, Object> settings = new HashMap<>();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/csl_web_app");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "");
+                settings.put(Environment.HBM2DDL_AUTO, "update");
+                settings.put(Environment.SHOW_SQL, true);
+                settings.put(Environment.FORMAT_SQL, true);
+
+//             settings.put(Environment.AUTOCOMMIT, true);
+
+
+                // c3p0 configuration
+                settings.put(Environment.C3P0_MIN_SIZE, 5);         //Minimum size of pool
+                settings.put(Environment.C3P0_MAX_SIZE, 20);        //Maximum size of pool
+                settings.put(Environment.C3P0_TIMEOUT, 10);       //Connection idle time in second
+                settings.put(Environment.C3P0_MAX_STATEMENTS, 150); //PreparedStatement cache size
+                settings.put(Environment.C3P0_CONFIG_PREFIX+".initialPoolSize", 6);
+
+                registryBuilder.applySettings(settings);
+
+                registry = registryBuilder.build();
+                MetadataSources sources = new MetadataSources(registry)
+                        .addAnnotatedClass(person.class);
+                Metadata metadata = sources.getMetadataBuilder().build();
+                sessionFactory = metadata.getSessionFactoryBuilder().build();
+            }
+            catch (Exception e) {
+                 if (registry != null) {
+                    StandardServiceRegistryBuilder.destroy(registry);
+                }
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+
+    public static void shutdown()
+    {
+        if (registry != null) {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+
+
+}
---
+++ src/main/java/com/hibernet/mainApp.java
@@ -0,0 +1,43 @@
+package com.hibernet;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.hibernet.entity.person;
+
+public class mainApp
+{
+    public static void main(String[] args)
+    {
+
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+
+            transaction = session.beginTransaction();
+
+            if(transaction.isActive()){
+                transaction.rollback();
+            }
+
+            transaction.begin();
+
+            person Person = new person();
+            Person.setName("farhanFuadRonok");
+            session.save(Person);
+            transaction.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        HibernateUtil.shutdown();
+    }
+}
